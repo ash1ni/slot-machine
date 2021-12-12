c1cb0819887984e05d47a5f243d89b2f79a8120f
stonks <ash1ni>
2021-12-12 10:08:56 +0530

    added initial functionality of slot machine

---
+++ script.js
@@ -0,0 +1,22 @@
+let value1 = document.getElementById('value1')
+let value2= document.getElementById('value2')
+let value3 = document.getElementById('value3')
+
+let values = [
+    '😀' , '😃' , '😄' , '😁', '🤣' , '😂', '😎', '😛'
+]
+
+
+function getRandomValue(){
+    return values[Math.floor(Math.random() * 8)]
+}
+
+
+
+setInterval(()=>{
+
+    value1.innerText = getRandomValue()
+    value2.innerText = getRandomValue()
+    value3.innerText = getRandomValue()
+
+}, 100)
