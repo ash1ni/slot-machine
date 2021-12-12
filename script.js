cdba294779961d2d47c23ea1e6409aab008f3c29
stonks <ash1ni>
2021-12-12 12:13:14 +0530

    added counter to increase and decrease the speed of machine

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ let value1 = document.getElementById('value1')
 let value2= document.getElementById('value2')
 let value3 = document.getElementById('value3')
 
+let inpSpeed = document.getElementById('inpSpeed')
+
+
 let values = [
     '😀' , '😃' , '😄' , '😁', '🤣' , '😂', '😎', '😛'
 ]
@@ -13,10 +16,24 @@ function getRandomValue(){
 
 
 
-setInterval(()=>{
+let animationId
+function updateAnimation(newSpeed){
+    if (animationId) clearInterval(animationId)
+
+    animationId = setInterval(()=>{
 
     value1.innerText = getRandomValue()
     value2.innerText = getRandomValue()
     value3.innerText = getRandomValue()
 
-}, 100)
+    }, 1000 / newSpeed)
+
+}
+
+inpSpeed.onchange = function(ev) {
+
+    // document.documentElement => this is root of css
+    document.documentElement.style.setProperty('--speed', ev.target.value)
+
+    updateAnimation(ev.target.value)
+}
